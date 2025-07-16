f61390b6213123f375469bfc68b225a950ab0a3b
Parveen Gartan <63051520+pga043@users.noreply.github.com>
2025-07-16 11:09:52 +0200

    Create install_miniconda.sh

---
+++ install_miniconda.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## x86 =>
+#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+#sh Miniconda3-latest-Linux-x86_64.sh -b
+#eval "$(~/miniconda3/bin/conda shell.bash hook)"
+#conda init
+
+
+## ARM =>
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O miniconda.sh
+#chmod +x miniconda.sh
+#bash miniconda.sh
